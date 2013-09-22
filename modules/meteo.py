e874093d70f87b4bb33e464c49eb6c14fa6fd0d1
Seraf <julien.syx@gmail.com>
2013-09-22 10:31:19 +0200

    Fixes

--- modules/meteo.py
+++ modules/meteo.py
@@ -22,8 +22,9 @@ class Meteo:
     def weatherAPI(self, city):
         r = requests.get('http://api.openweathermap.org/data/2.5/weather?', params={
             'lang': self.configuration_lisa['lang'],
-            'city': ','.join([city,self.configuration_lisa['lang']]),
+            'q': ','.join([city,self.configuration_lisa['lang']]),
         })
+        print r.url
         if r.ok:
             return r.json()
         else:
@@ -35,19 +36,21 @@ class Meteo:
         else:
             city = self.configuration['configuration']['city']
         weather = self.weatherAPI(city)
-        if not weather['problem']:
-            if self.configuration['configuration']['temperature'] == "celcius":
-                temptext = "temp_celius"
-                temp = (((weather['main']['temp'] - 32) * 9) / 5)
+        if "problem" not in weather:
+            print weather
+            if self.configuration['configuration']['temperature'] == "celsius":
+                temptext = _("temp_celsius")
+                temp = (((weather['main']['temp'] - 32) * 5) / 9)
             else:
-                temptext = "temp_fahrenheit"
+                temptext = _("temp_fahrenheit")
                 temp = weather['main']['temp']
+            print temptext
             body = ", ".join([
                 _('weather in city') % city,
-                _('climat') % weather['weather']['description'],
-                _(temptext) % temp,
-                _('humidity') % weather['main']['humidity'],
-                _('wind speed') % ['wind']['speed']
+                _('climat') % weather['weather'][0]['description'],
+                temptext % str(temp),
+                _('humidity') % str(weather['main']['humidity']),
+                _('wind speed') % str(weather['wind']['speed'])
             ])
         else:
             body = weather['problem']
